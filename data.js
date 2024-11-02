31ea7f1eb32e26be07f7c89487359bf2586444a2
Andrew-A10 <andrew.armstrong@malad.us>
2024-11-02 13:34:20 -0600

    shhhhh

---
+++ data.js
@@ -0,0 +1,15 @@
+var threads = [
+    {
+        title: "Thread 1",
+        author: "Andrew",
+        date: Date.now(),
+        content: "Thread content",
+        comments: [
+            {
+                author: "Andrew",
+                date: Date.now(),
+                content: "Hola"
+            }
+        ]
+    }
+]
